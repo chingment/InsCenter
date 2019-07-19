b9da9773ca4afb0a09f3de1bd15a11de34a7fb3e
jiecong <jiecong@outlook.com>
2019-07-19 17:53:17 +0800

    .

---
+++ LocalS.Service/Api/Merch/UserService.cs
@@ -0,0 +1,39 @@
+﻿using LocalS.BLL;
+using Lumos;
+using Lumos.DbRelay;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalS.Service.Api.Merch
+{
+    public class UserService : BaseDbContext
+    {
+        public CustomJsonResult LoginByAccount(RopUserLoginByAccount rop)
+        {
+            var result = new CustomJsonResult();
+            var ret = new RetUserLoginByAccount();
+
+            var merchantUser = CurrentDb.SysMerchantUser.Where(m => m.UserName == rop.UserName).FirstOrDefault();
+
+            if (merchantUser==null)
+            {
+                return new CustomJsonResult(ResultType.Failure, ResultCode.Failure, "账号不存在");
+            }
+
+            if (!PassWordHelper.VerifyHashedPassword(merchantUser.PasswordHash, rop.Password))
+            {
+                return new CustomJsonResult(ResultType.Failure, ResultCode.Failure, "账号密码不正确"); 
+            }
+
+            ret.MId = merchantUser.MerchantId;
+            ret.UId = merchantUser.Id;
+
+            result = new CustomJsonResult(ResultType.Success, ResultCode.Success, "获取成功", ret);
+
+            return result;
+        }
+    }
+}
---
+++ WebApiMerch/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using LocalS.Service.Api.Merch;
+using Lumos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiMerch.Controllers
+{
+    public class UserController : ApiController
+    {
+        [HttpPost]
+        public OwnApiHttpResponse LoginByAccount(RopUserLoginByAccount rop)
+        {
+            IResult result = MerchServiceFactory.User.LoginByAccount(rop);
+
+            return new OwnApiHttpResponse(result);
+        }
+    }
+}
