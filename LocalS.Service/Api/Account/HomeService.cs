5381fd23c92601de836e870debd0825e5aa1f469
jiecong <jiecong@outlook.com>
2019-07-31 17:57:26 +0800

    .

--- LocalS.Service/Api/Account/HomeService.cs
+++ LocalS.Service/Api/Account/HomeService.cs
@@ -1,4 +1,5 @@
-﻿using Lumos;
+﻿using LocalS.BLL;
+using Lumos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace LocalS.Service.Api.Account
 {
-    public class HomeService
+    public class HomeService : BaseDbContext
     {
         public CustomJsonResult GetIndexPageData(string operater, string userId)
         {
@@ -16,10 +17,18 @@ namespace LocalS.Service.Api.Account
             var ret = new RetHomeGetIndexPageData();
 
 
+            var sysUser = CurrentDb.SysUser.Where(m => m.Id == userId).FirstOrDefault();
 
-            ret.Appcaltions.Add(new RetHomeGetIndexPageData._Appcaltion() { Name = "商户代理系统", Url = "http://merch.ins-uplink.com/", ImgUrl="http://file.17fanju.com/Upload/img_merch.png", Describe = "商家客户使用", });
-            ret.Appcaltions.Add(new RetHomeGetIndexPageData._Appcaltion() { Name = "后台管理系统", Url = "http://admin.ins-uplink.com/", ImgUrl = "http://file.17fanju.com/Upload/img_admin.png", Describe = "后端用户，公司内部使用", });
-
+            switch(sysUser.BelongSite)
+            {
+                case Lumos.DbRelay.Enumeration.BelongSite.Merch:
+                    ret.Appcaltions.Add(new RetHomeGetIndexPageData._Appcaltion() { Name = "商户代理系统", Url = "http://merch.ins-uplink.com/", ImgUrl = "http://file.17fanju.com/Upload/img_merch.png", Describe = "商家客户使用", });
+                    break;
+                case Lumos.DbRelay.Enumeration.BelongSite.Admin:
+                    ret.Appcaltions.Add(new RetHomeGetIndexPageData._Appcaltion() { Name = "后台管理系统", Url = "http://admin.ins-uplink.com/", ImgUrl = "http://file.17fanju.com/Upload/img_admin.png", Describe = "后端用户，公司内部使用", });
+                    break;
+            }
+           
             result = new CustomJsonResult(ResultType.Success, ResultCode.Success, "获取成功", ret);
 
             return result;
