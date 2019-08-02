7538218348a54f2ce6325d4cc14e563315b9e392
jiecong <jiecong@outlook.com>
2019-08-02 17:59:39 +0800

    .

--- WebAdmin/src/views/adminuser/add.vue
+++ WebAdmin/src/views/adminuser/add.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="useradd_container" class="app-container">
-    <el-form ref="form" :model="form" :rules="rules" label-width="75px">
+    <el-form ref="form" :model="form" :rules="rules" label-width="80px">
       <el-form-item label="用户名" prop="userName">
         <el-input v-model="form.userName" />
       </el-form-item>
@@ -9,6 +9,12 @@
       </el-form-item>
       <el-form-item label="姓名" prop="fullName">
         <el-input v-model="form.fullName" />
+      </el-form-item>
+      <el-form-item label="所属机构" prop="fullName">
+          <el-cascader
+    :options="cascader_options"
+    :props="cascader_props"
+    clearable></el-cascader>
       </el-form-item>
       <el-form-item label="手机号码" prop="phoneNumber">
         <el-input v-model="form.phoneNumber" />
@@ -24,6 +30,7 @@
 </template>
 
 <script>
+// https://element.eleme.cn/#/zh-CN/component/cascader
 import { MessageBox } from 'element-ui'
 import { addUser } from '@/api/adminuser'
 import fromReg from '@/utils/formReg'
@@ -44,7 +51,55 @@ export default {
         fullName: [{ required: true, message: '必填', trigger: 'change' }],
         phoneNumber: [{ required: false, message: '格式错误,eg:13800138000', trigger: 'change', pattern: fromReg.phoneNumber }],
         email: [{ required: false, message: '格式错误,eg:xxxx@xxx.xxx', trigger: 'change', pattern: fromReg.email }]
-      }
+      },
+      cascader_props: { multiple: true },
+      cascader_options: [{
+          value: 1,
+          label: '东南',
+          children: [{
+            value: 2,
+            label: '上海',
+            children: [
+              { value: 3, label: '普陀' },
+              { value: 4, label: '黄埔' },
+              { value: 5, label: '徐汇' }
+            ]
+          }, {
+            value: 7,
+            label: '江苏',
+            children: [
+              { value: 8, label: '南京' },
+              { value: 9, label: '苏州' },
+              { value: 10, label: '无锡' }
+            ]
+          }, {
+            value: 12,
+            label: '浙江',
+            children: [
+              { value: 13, label: '杭州' },
+              { value: 14, label: '宁波' },
+              { value: 15, label: '嘉兴' }
+            ]
+          }]
+        }, {
+          value: 17,
+          label: '西北',
+          children: [{
+            value: 18,
+            label: '陕西',
+            children: [
+              { value: 19, label: '西安' },
+              { value: 20, label: '延安' }
+            ]
+          }, {
+            value: 21,
+            label: '新疆维吾尔族自治区',
+            children: [
+              { value: 22, label: '乌鲁木齐' },
+              { value: 23, label: '克拉玛依' }
+            ]
+          }]
+        }]
     }
   },
   methods: {
